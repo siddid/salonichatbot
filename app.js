f652415010a02773e1c27858f093caba69793a4c
Rajesh Gupta 2 <rgup49@sapient.com>
2017-02-23 22:25:56 -0500

    changes

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: 'f91b8de4-e3ea-4f19-bf12-cca75ea8a80c',
+    appPassword: 'fmNWirQjNLkkwqeznPayd3R'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
